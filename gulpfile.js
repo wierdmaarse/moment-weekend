afe301cd72a43cfdf96aded361a4c75dfeccd6d7
Jonathon Choo <jonno.choo@gmail.com>
2015-08-12 00:03:58 +1000

    Added single quote ESLint rule

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 
 gulp.task('lint', function () {
-  return gulp.src(['weekend.js'])
+  return gulp.src(['weekend.js', 'specs/weekendSpec.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
--- specs/weekendSpec.js
+++ specs/weekendSpec.js
@@ -1,42 +1,42 @@
 var weekend = require('../weekend');
 var moment = require('moment');
 
-describe("weekend", function(){
+describe('weekend', function(){
 
-  it("start date is not a moment should return error", function(){
-    var start = "2015-02-12";
+  it('start date is not a moment should return error', function(){
+    var start = '2015-02-12';
     expect(function() { weekend.diff(start, null)})
-      .toThrow(new Error("start date is not a moment"));
+      .toThrow(new Error('start date is not a moment'));
   });
 
-  it("end date is not a moment should return error", function(){
-    var start = moment("2015-02-12", "YYYY-MM-DD")
+  it('end date is not a moment should return error', function(){
+    var start = moment('2015-02-12', 'YYYY-MM-DD')
     expect(function() { weekend.diff(start, null)})
-      .toThrow(new Error("end date is not a moment"));
+      .toThrow(new Error('end date is not a moment'));
   });
 
-  it("test", function(){
-    var start = moment("2015-02-12", "YYYY-MM-DD")
-    var end = moment("2015-02-17", "YYYY-MM-DD")
+  it('test', function(){
+    var start = moment('2015-02-12', 'YYYY-MM-DD')
+    var end = moment('2015-02-17', 'YYYY-MM-DD')
     var result = weekend.diff(start, end);
 
     expect(result).toBe(3);
   });
 
-  it("should say its weekend for a saturday", function(){
-    var date = moment("2015-07-04", "YYYY-MM-DD")
+  it('should say its weekend for a saturday', function(){
+    var date = moment('2015-07-04', 'YYYY-MM-DD')
     var result = weekend.isWeekend(date);
     expect(result).toBe(true);
   });
 
-  it("should say its weekend for a sunday", function(){
-    var date = moment("2015-07-05", "YYYY-MM-DD")
+  it('should say its weekend for a sunday', function(){
+    var date = moment('2015-07-05', 'YYYY-MM-DD')
     var result = weekend.isWeekend(date);
     expect(result).toBe(true);
   });
 
-  it("should say it isnt weekend for a monday", function(){
-    var date = moment("2015-07-06", "YYYY-MM-DD")
+  it('should say it isnt weekend for a monday', function(){
+    var date = moment('2015-07-06', 'YYYY-MM-DD')
     var result = weekend.isWeekend(date);
     expect(result).toBe(false);
   });
