d3e1e0f5c0653e10452e531d35267c401c28389b
Jonathon Choo <jonno.choo@gmail.com>
2015-08-11 23:56:18 +1000

    Added ESLint and modified code to comply to rules

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+
+gulp.task('lint', function () {
+  return gulp.src(['weekend.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
+gulp.task('default', ['lint'], function () {
+    // This will only run if the lint task is successful...
+});
--- weekend.js
+++ weekend.js
@@ -1,34 +1,38 @@
 var moment = require('moment');
 
-var diff = function(startDate, endDate) {
-    if(!moment.isMoment(startDate))
-      throw new Error('start date is not a moment');
-
-    if(!moment.isMoment(endDate))
-      throw new Error('end date is not a moment');
-
-    if(startDate > endDate)
-      throw new Error('start date cannot be greater than end date');
-
-    var pattern = 'DD-MM-YYYY';
-    var dateIterator = moment(startDate);
-    var index = 0;
-    while(dateIterator.format(pattern) != endDate.format(pattern)){
-
-      if(!isWeekend(dateIterator)){
-        index++;
-      }
-
-      dateIterator.add(1, 'days');
-    }
-    return index;
-  };
-
-var isWeekend = function (date){
-  return date.isoWeekday() >= 6;
-}
-
 module.exports = {
   diff: diff,
   isWeekend: isWeekend
 };
+
+function diff(startDate, endDate) {
+  if(!moment.isMoment(startDate)) {
+    throw new Error('start date is not a moment');
+  }
+
+  if(!moment.isMoment(endDate)) {
+    throw new Error('end date is not a moment');
+  }
+
+  if(startDate > endDate) {
+    throw new Error('start date cannot be greater than end date');
+  }
+
+  var pattern = 'DD-MM-YYYY';
+  var dateIterator = moment(startDate);
+  var index = 0;
+  while(dateIterator.format(pattern) != endDate.format(pattern)){
+
+    if(!isWeekend(dateIterator)){
+      index++;
+    }
+
+    dateIterator.add(1, 'days');
+  }
+  return index;
+}
+
+function isWeekend(date){
+  return date.isoWeekday() >= 6;
+}
+
